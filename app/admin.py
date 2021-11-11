9c5aa2850692459eec40cf1c38e2765e4888727c
ahmedjorge <ahmedferreira11@gmail.com>
2021-11-11 15:41:33 +0000

    [Migrate] heroku create module Student

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from app.models import Student
+
+
+@admin.register(Student)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('number', 'name', 'age', 'grade')
--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    number = models.IntegerField(verbose_name="number")
+    name = models.CharField(verbose_name="name", max_length=100)
+    age = models.IntegerField(verbose_name="age")
+    grade = models.FloatField(verbose_name="grade")
+
+    class Meta:
+        verbose_name = "student"
+        verbose_name_plural = "students"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
